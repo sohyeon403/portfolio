87efdf375b43ce52c93e2d42ae23e557add4bcab
sohyeon403 <sohyeon403>
2020-12-01 00:06:27 +0900

    update[fix] : popup.js

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,7 @@
         overlay.show();
         overlay.appendTo(document.body);
         $('.popup_wrap').show();
-        $('.close').click(function () {
+        $('.close').click(function(){
             $('.popup_text').animate({
                 'top': '280px'
             }, 200).delay(100).animate({
@@ -14,9 +14,19 @@
 
 
 
-            const chrome = $('<div id="chrome"></div>');
-            chrome.show();
-            overlay.appendTo(document.body);
+        });
 
+        const chrome = $('<div id="chrome"></div>');
+        chrome.show();
+        overlay.appendTo(document.body);
+        $('.popup_wrap').show();
+        $('.btn_close').click(function(){
+            $('.popup_chrome').animate({
+                'left':'100px'
+            }, 200).delay(100).animate({
+                'left':'-2200px',
+                'opacity':'.5'
+            },400);
+            return false;
         });
     });
